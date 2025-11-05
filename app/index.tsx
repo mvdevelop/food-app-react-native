348e590647640dbe7fd31c94aa42ac7511724bb5
mvdevelop <marcosvmdilly@gmail.com>
2025-11-05 10:25:51 -0300

    working on layout.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,9 @@ import { Text, View } from "react-native";
  
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-100">
-      <Text className="text-xl font-bold text-red-100">
-        Welcome to Nativewind!
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Foods App!
       </Text>
     </View>
   );
